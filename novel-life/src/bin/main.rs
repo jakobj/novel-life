a92f3975879d45b43c42a57a870c4bc713de9345
Jakob Jordan <jakobjordan@posteo.de>
2023-04-07 15:12:36 +0200

    Small improvements

--- novel-life/src/bin/main.rs
+++ novel-life/src/bin/main.rs
@@ -1,23 +1,49 @@
-// use std::io::Write;
-// use crossterm::{cursor, ExecutableCommand};
+use std::io::Write;
+use crossterm::{cursor, ExecutableCommand};
 
-use novel_life::{individual::Individual, universe::Universe};
+use novel_life::{individual::Individual, lcell::LCell, universe::{Universe, self}};
 
 fn main() {
+    let universe_size = 50;
+    let seed_size = 5;
+    let n_ea_steps = 50;
+    let n_simulation_steps = 50;
+
+    let discoveries = novelty_search(universe_size, seed_size, n_ea_steps, n_simulation_steps);
+
+    for cells in discoveries {
+        let u = Universe::new(universe_size);
+        let offset = universe_size / 2 - seed_size / 2;
+        let u = u.seed(&cells, offset, offset);
+        let history = u.simulate_with_history(500);
+        visualize(history, universe_size);
+    }
+}
+
+fn novelty_search(
+    universe_size: usize,
+    seed_size: usize,
+    n_ea_steps: usize,
+    n_simulation_steps: usize,
+) -> Vec<Vec<Vec<LCell>>> {
+    fn interesting(u: &Universe) -> bool {
+        u.n_alive() > 100
+    }
+
+    let mut discoveries = Vec::new();
     let mut archive = Vec::new();
-    let mut parents = vec![Individual::new(5); 4];
-    for _ in 0..50 {
+    let mut parents = vec![Individual::new(seed_size); 4];
+    for _ in 0..n_ea_steps {
         let mut offspring = parents.clone();
         for i in 0..offspring.len() {
             offspring[i] = offspring[i].mutate(1);
 
-            let universe = Universe::new(100);
-            let universe = universe.seed(&offspring[i].cells, 48, 48);
-            let universe = universe.simulate(500);
-            if universe.n_alive() > 100 {
-                println!("{}", universe.n_alive());
-                println!("{:?}", offspring[i].cells);
-                println!("{}", universe);
+            let universe = Universe::new(universe_size);
+            let offset = universe_size / 2 - seed_size / 2;
+            let universe = universe.seed(&offspring[i].cells, offset, offset);
+            let universe = universe.simulate(n_simulation_steps);
+            if interesting(&universe) {
+                discoveries.push(offspring[i].cells.clone());
             }
 
             let mut distances = archive.iter().map(|u: &Universe| u.distance(&universe)).collect::<Vec<u32>>();
@@ -34,32 +60,20 @@ fn main() {
 
         parents = population[..4].to_vec();
     }
+    discoveries
 }
 
-// fn simulate_with_history(u: &Universe, k: usize) -> Vec<Universe> {
-//     let mut history = Vec::new();
-//     let mut u = u.clone();
-//     history.push(u.clone());
-//     for _ in 0..k {
-//         u = u.tick();
-//         history.push(u.clone())
-//     }
-//     history
-// }
-
-// fn visualize(history: Vec<Universe>) {
-//     let mut stdout = std::io::stdout();
-//     for u in history {
-//         let s = u.to_string();
-//         println!("{:?}", s);
-//         // for v in u.cells {
-//         //     writeln!(stdout, "{}", v.iter().map(|c| c.to_string()).collect::<String>()).unwrap();
-//         // }
-//         // std::thread::sleep(std::time::Duration::from_millis(200));
-//         // if !first_line {
-//         //     stdout.execute(cursor::MoveUp(n_rows as u16)).unwrap();
-//         // } else {
-//         //     first_line = false;
-//         // }
-//     }
-// }
+fn visualize(history: Vec<Universe>, universe_size: usize) {
+    let mut stdout = std::io::stdout();
+    let n = history.len();
+    for (i, u) in history.into_iter().enumerate() {
+        let s = u.to_string();
+        for r in s.lines() {
+            writeln!(stdout, "{}", r).unwrap();
+        }
+        std::thread::sleep(std::time::Duration::from_millis(100));
+        if i != n - 1 {
+            stdout.execute(cursor::MoveUp(universe_size as u16)).unwrap();
+        }
+    }
+}
--- novel-life/src/universe.rs
+++ novel-life/src/universe.rs
@@ -6,8 +6,8 @@ pub struct Universe {
 }
 
 impl Universe {
-    pub fn new(n: usize) -> Self {
-        Self{ cells: vec![vec![LCell::Dead; n]; n] }
+    pub fn new(size: usize) -> Self {
+        Self{ cells: vec![vec![LCell::Dead; size]; size] }
     }
 
     pub fn distance(&self, other: &Self) -> u32 {
@@ -47,11 +47,22 @@ impl Universe {
         u
     }
 
+    pub fn simulate_with_history(&self, k: usize) -> Vec<Self> {
+        let mut history = Vec::new();
+        let mut u = self.clone();
+        history.push(u.clone());
+        for _ in 0..k {
+            u = u.tick();
+            history.push(u.clone())
+        }
+        history
+    }
+
     pub fn tick(&self) -> Self {
         let mut u = self.clone();
-        let n = u.cells.len() as i32;
-        for row in 0..n {
-            for col in 0..n {
+        let size = u.cells.len() as i32;
+        for row in 0..size {
+            for col in 0..size {
                 let mut n_alive = 0;
                 for delta_y in [-1, 0, 1] {
                     for delta_x in [-1, 0, 1] {
@@ -60,7 +71,7 @@ impl Universe {
                         }
                         let y = row + delta_y;
                         let x = col + delta_x;
-                        if (y >= 0 && y < n) && (x >= 0 && x < n) {
+                        if (y >= 0 && y < size) && (x >= 0 && x < size) {
                             if self.cells[y as usize][x as usize] == LCell::Alive {
                                 n_alive += 1;
                             }
@@ -80,23 +91,14 @@ impl Universe {
         }
         u
     }
-
-    // fn from_map_str(s: &str) -> Self {
-    //     let cells = s.lines().map(|l| l.chars().map(|c| LCell::from(c) ).collect::<Vec<LCell>>()).collect::<Vec<Vec<LCell>>>();
-    //     let n_cols = cells[0].len();
-    //     for v in cells.iter() {
-    //         assert!(v.len() == n_cols);
-    //     }
-    //     Universe{ cells }
-    // }
 }
 
 impl From<&str> for Universe {
     fn from(s: &str) -> Self {
         let cells = s.lines().map(|l| l.chars().map(|c| LCell::from(c) ).collect::<Vec<LCell>>()).collect::<Vec<Vec<LCell>>>();
-        let n = cells.len();
+        let size = cells.len();
         for v in cells.iter() {
-            assert!(v.len() == n);
+            assert!(v.len() == size);
         }
         Universe{ cells }
     }
