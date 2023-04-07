1e55b2c7d75316f5ffc550da955f21f02132b09a
Jakob Jordan <jakobjordan@posteo.de>
2023-04-07 16:54:41 +0200

    Fix ea

--- novel-life/src/bin/main.rs
+++ novel-life/src/bin/main.rs
@@ -1,4 +1,5 @@
 use crossterm::{cursor, ExecutableCommand};
+use rand::{distributions::Uniform, prelude::Distribution};
 use std::io::Write;
 
 use novel_life::{
@@ -10,7 +11,7 @@ use novel_life::{
 fn main() {
     let universe_size = 52;
     let seed_size = 3;
-    let n_ea_steps = 1_000;
+    let n_ea_steps = 200;
     let n_simulation_steps = 500;
 
     let discoveries = novelty_search(universe_size, seed_size, n_ea_steps, n_simulation_steps);
@@ -34,38 +35,49 @@ fn novelty_search(
         u.n_alive() > 20 && u.symmetry() > 10
     }
 
+    let n_offspring = 6;
+
     let mut discoveries = Vec::new();
     let mut archive = Vec::new();
     let mut parents = vec![Individual::new(seed_size); 4];
+    let mut rng = rand::thread_rng();
+    let between = Uniform::from(0..parents.len());
     for _ in 0..n_ea_steps {
-        let mut offspring = parents.clone();
-        for i in 0..offspring.len() {
-            offspring[i] = offspring[i].mutate(1);
+        let mut offspring = Vec::with_capacity(n_offspring);
 
+        for _ in 0..n_offspring {
+
+            // choose a random parent and mutate
+            let idx = between.sample(&mut rng);
+            let mut ind = parents[idx].mutate(1);
+
+            // compute final state of universe
             let universe = Universe::new(universe_size);
             let offset = universe_size / 2 - seed_size / 2;
-            let universe = universe.seed(&offspring[i].cells, offset, offset);
+            let universe = universe.seed(&ind.cells, offset, offset);
             let universe = universe.simulate(n_simulation_steps);
             if interesting(&universe) {
-                discoveries.push(offspring[i].cells.clone());
+                discoveries.push(ind.cells.clone());
             }
 
+            // compute novelty as total distance from four closest neighbors
             let mut distances = archive
                 .iter()
                 .map(|u: &Universe| u.distance(&universe))
                 .collect::<Vec<u32>>();
             distances.sort_unstable();
             if distances.len() >= 4 {
-                offspring[i].novelty = distances[..4].iter().sum::<u32>();
+                ind.novelty = distances[..4].iter().sum::<u32>();
             }
 
+            offspring.push(ind);
             archive.push(universe);
         }
-        let mut population = offspring.clone();
-        population.append(&mut parents);
-        population.sort_unstable_by_key(|a| a.novelty);
 
-        parents = population[..4].to_vec();
+        // choose offspring with highest novelty
+        offspring.sort_by_key(|a| a.novelty);
+        offspring.reverse();
+        parents = offspring[..4].to_vec();
     }
     discoveries
 }
